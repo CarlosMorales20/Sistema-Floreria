2f0c2b4dbf189504d6a61f37ae5b23f395c00b7b
Daen-Beltran-Morales <daenmorales878@gmail.com>
2026-07-23 17:54:34 -0600

    Se agrega formulario de login

---
+++ FloreriaOrquideas2/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Floreria
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            checkBox1 = new CheckBox();
+            btnIniciarSesion = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(56, 215);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Inicia sesion para continuar";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 258);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 299);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(90, 255);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(159, 23);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(90, 296);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(159, 23);
+            txtContraseña.TabIndex = 4;
+            txtContraseña.UseSystemPasswordChar = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(90, 338);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(140, 19);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "Mostrar la contraseña";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // btnIniciarSesion
+            // 
+            btnIniciarSesion.BackColor = Color.DeepPink;
+            btnIniciarSesion.FlatStyle = FlatStyle.Flat;
+            btnIniciarSesion.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnIniciarSesion.ForeColor = SystemColors.ButtonHighlight;
+            btnIniciarSesion.Location = new Point(90, 372);
+            btnIniciarSesion.Name = "btnIniciarSesion";
+            btnIniciarSesion.Size = new Size(113, 30);
+            btnIniciarSesion.TabIndex = 6;
+            btnIniciarSesion.Text = "Iniciar Sesion";
+            btnIniciarSesion.UseVisualStyleBackColor = false;
+            btnIniciarSesion.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(56, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(193, 194);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(300, 453);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnIniciarSesion);
+            Controls.Add(checkBox1);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private CheckBox checkBox1;
+        private Button btnIniciarSesion;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ FloreriaOrquideas2/Form1.cs
@@ -0,0 +1,39 @@
+namespace Floreria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtContraseña.UseSystemPasswordChar = !checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "admin" && txtContraseña.Text == "12345")
+            {
+                MenuPrincipal menu = new MenuPrincipal();
+                menu.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Datos incorrectos");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
