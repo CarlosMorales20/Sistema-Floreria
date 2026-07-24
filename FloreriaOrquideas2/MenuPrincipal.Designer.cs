8efcdec794261def12c73dcad33706bf54bba1d7
Daen-Beltran-Morales <daenmorales878@gmail.com>
2026-07-23 18:10:41 -0600

    Se agrega un menu principal

---
+++ FloreriaOrquideas2/MenuPrincipal.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Floreria
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelMenu = new Panel();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            btnVentas1 = new Button();
+            btnProductos = new Button();
+            button1 = new Button();
+            panelSuperior = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            panelProductos = new Panel();
+            label4 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            button8 = new Button();
+            panelVentas = new Panel();
+            pictureBox3 = new PictureBox();
+            panelInventario = new Panel();
+            pictureBox2 = new PictureBox();
+            panelReportes = new Panel();
+            pictureBox4 = new PictureBox();
+            panelMenu.SuspendLayout();
+            panelSuperior.SuspendLayout();
+            panelProductos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panelVentas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panelInventario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panelReportes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.WhiteSmoke;
+            panelMenu.Controls.Add(button6);
+            panelMenu.Controls.Add(button5);
+            panelMenu.Controls.Add(button4);
+            panelMenu.Controls.Add(btnVentas1);
+            panelMenu.Controls.Add(btnProductos);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(200, 531);
+            panelMenu.TabIndex = 0;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.MediumSeaGreen;
+            button6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(-3, 446);
+            button6.Name = "button6";
+            button6.Size = new Size(200, 70);
+            button6.TabIndex = 5;
+            button6.Text = "⎋ Salir";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.MediumSeaGreen;
+            button5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(-3, 381);
+            button5.Name = "button5";
+            button5.Size = new Size(200, 70);
+            button5.TabIndex = 4;
+            button5.Text = "▤ Reportes";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.MediumSeaGreen;
+            button4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(-3, 314);
+            button4.Name = "button4";
+            button4.Size = new Size(200, 70);
+            button4.TabIndex = 3;
+            button4.Text = "▣ Inventario\n";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // btnVentas1
+            // 
+            btnVentas1.BackColor = Color.MediumSeaGreen;
+            btnVentas1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnVentas1.Location = new Point(-3, 249);
+            btnVentas1.Name = "btnVentas1";
+            btnVentas1.Size = new Size(200, 70);
+            btnVentas1.TabIndex = 2;
+            btnVentas1.Text = "\U0001f6d2 Ventas";
+            btnVentas1.UseVisualStyleBackColor = false;
+            btnVentas1.Click += button3_Click;
+            // 
+            // btnProductos
+            // 
+            btnProductos.BackColor = Color.MediumSeaGreen;
+            btnProductos.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnProductos.Location = new Point(-3, 183);
+            btnProductos.Name = "btnProductos";
+            btnProductos.Size = new Size(200, 70);
+            btnProductos.TabIndex = 1;
+            btnProductos.Text = "✿ Productos";
+            btnProductos.UseVisualStyleBackColor = false;
+            btnProductos.Click += btnProductos_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SeaGreen;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(-3, 117);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 70);
+            button1.TabIndex = 0;
+            button1.Text = "⌂ Inicio";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panelSuperior
+            // 
+            panelSuperior.Controls.Add(label2);
+            panelSuperior.Controls.Add(label1);
+            panelSuperior.Dock = DockStyle.Top;
+            panelSuperior.Location = new Point(0, 0);
+            panelSuperior.Name = "panelSuperior";
+            panelSuperior.Size = new Size(817, 120);
+            panelSuperior.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(202, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(284, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Sistema de control de inventario";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 32.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.ForestGreen;
+            label1.Location = new Point(203, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(283, 49);
+            label1.TabIndex = 0;
+            label1.Text = "ORQUIDEAS";
+            // 
+            // panelProductos
+            // 
+            panelProductos.BackColor = Color.MistyRose;
+            panelProductos.Controls.Add(label4);
+            panelProductos.Controls.Add(label3);
+            panelProductos.Controls.Add(pictureBox1);
+            panelProductos.Controls.Add(button8);
+            panelProductos.Location = new Point(203, 126);
+            panelProductos.Name = "panelProductos";
+            panelProductos.Size = new Size(300, 150);
+            panelProductos.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(116, 88);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Administrar flores y productos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(129, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Productos";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(20, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(80, 80);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Transparent;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 0, 0, 0);
+            button8.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 0, 0, 0);
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(3, 9);
+            button8.Name = "button8";
+            button8.Size = new Size(294, 124);
+            button8.TabIndex = 1;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // panelVentas
+            // 
+            panelVentas.Controls.Add(pictureBox3);
+            panelVentas.Location = new Point(509, 126);
+            panelVentas.Name = "panelVentas";
+            panelVentas.Size = new Size(300, 150);
+            panelVentas.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(22, 30);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(80, 80);
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // panelInventario
+            // 
+            panelInventario.Controls.Add(pictureBox2);
+            panelInventario.Location = new Point(203, 282);
+            panelInventario.Name = "panelInventario";
+            panelInventario.Size = new Size(300, 150);
+            panelInventario.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(20, 32);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(80, 80);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panelReportes
+            // 
+            panelReportes.Controls.Add(pictureBox4);
+            panelReportes.Location = new Point(509, 282);
+            panelReportes.Name = "panelReportes";
+            panelReportes.Size = new Size(300, 150);
+            panelReportes.TabIndex = 5;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(22, 32);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(80, 80);
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // MenuPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(817, 514);
+            Controls.Add(panelReportes);
+            Controls.Add(panelInventario);
+            Controls.Add(panelVentas);
+            Controls.Add(panelProductos);
+            Controls.Add(panelSuperior);
+            Controls.Add(panelMenu);
+            Name = "MenuPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MenuPrincipal";
+            panelMenu.ResumeLayout(false);
+            panelSuperior.ResumeLayout(false);
+            panelSuperior.PerformLayout();
+            panelProductos.ResumeLayout(false);
+            panelProductos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panelVentas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panelInventario.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panelReportes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelMenu;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button btnVentas1;
+        private Button btnProductos;
+        private Button button1;
+        private Panel panelSuperior;
+        private Label label2;
+        private Label label1;
+        private Panel panelProductos;
+        private Panel panelVentas;
+        private Panel panelInventario;
+        private Panel panelReportes;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private Button button8;
+        private Label label4;
+        private Label label3;
+    }
+}
---
+++ FloreriaOrquideas2/MenuPrincipal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Floreria
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Ventas ven = new Ventas();
+            ven.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Productos prod = new Productos();
+            prod.Show();
+            this.Hide();
+        }
+
+        private void btnProductos_Click(object sender, EventArgs e)
+        {
+            Productos prod = new Productos();
+            prod.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Reportes rep = new Reportes();
+            rep.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Inventario Inv = new Inventario();
+            Inv.Show();
+            this.Hide();
+        }
+    }
+}
